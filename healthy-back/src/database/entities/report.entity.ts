4f9191f4df2ae857871cb704f0ff39965455e8c2
ash <tkdgys1234@gamil.com>
2025-04-01 17:04:29 +0900

    경로 변경

---
+++ healthy-back/src/database/entities/report.entity.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { User } from './user.entity';
+
+@Entity()
+export class Report {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'longtext' })
+  report: string;
+
+  @Column({ default: 0 })
+  reportCnt: number;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  timestamp: Date;
+
+  // 신고당한 사람
+  @ManyToOne(() => User, (user) => user.reports, { onDelete: 'CASCADE' })
+  user: User;
+
+  // 신고한 사람
+  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  reporter: User; // 신고자 (이 사람은 게시글을 신고한 사람)
+}
