19b83bdc593a7b69077be4415eb482946cc57524
ash <tkdgys1234@gmail.com>
2025-04-02 23:57:16 +0900

    결제 검증까지, 나머진 시크릿 키 필요

--- healthy-back/src/app.module.ts
+++ healthy-back/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { LogModule } from './log/log.module';
 import { AiModule } from './ai/ai.module';
+import { PayModule } from './pay/pay.module';
 
 @Module({
-  imports: [DatabaseModule, LogModule, AiModule],
+  imports: [DatabaseModule, LogModule, AiModule, PayModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ healthy-back/src/pay/pay.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Controller,
+  Get,
+  Query,
+  Res,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { PayService } from './pay.service';
+import { Response } from 'express';
+
+@Controller('payment')
+export class PaymentController {
+  constructor(private readonly paymentService: PayService) {}
+
+  @Get('success')
+  async paymentSuccess(
+    @Query('paymentKey') paymentKey: string,
+    @Query('orderId') orderId: string,
+    @Query('amount') amount: string,
+    @Res() res: Response,
+  ) {
+    try {
+      // 결제 검증 (결제 금액과 주문 ID가 올바른지 확인)
+      const payRes = await this.paymentService.verifyPay(
+        paymentKey,
+        orderId,
+        amount,
+      );
+
+      if (payRes.success) {
+        // 결제 성공 시 처리 (DB 업데이트, 영수증 발행 등)
+        res.redirect(
+          `http://localhost:3000/payment/success?orderId=${orderId}`,
+        );
+      } else {
+        throw new HttpException(
+          'Payment verification failed',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+    } catch (error) {
+      console.error(error);
+      res.redirect(
+        `http://localhost:3000/payment/fail?message=${error.message}`,
+      );
+    }
+  }
+
+  //결제 실패
+  @Get('fail')
+  paymentFail(@Query() query, @Res() res: Response) {
+    console.error('Payment failed:', query);
+    res.redirect(`http://localhost:3000/payment/fail?message=${query.message}`);
+  }
+}
