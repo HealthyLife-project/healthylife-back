03cf4545479ac23b2995a93667b08c4ec08fea86
ash <tkdgys1234@gmail.com>
2025-04-07 19:24:27 +0900

    InbodyDto 타입 수정

--- healthy-back/src/database/entities/dto/inbodydtop.ts
+++ healthy-back/src/database/entities/dto/inbodydtop.ts
@@ -4,27 +4,31 @@ import { IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateInbodyDto {
-  @ApiProperty({ example: '70.5', description: '몸무게 (kg)' })
+  @ApiProperty({ example: '70.5', description: '몸무게 (kg)', type: String })
   @IsString()
   weight: string;
 
-  @ApiProperty({ example: '30.2', description: '골격근량 (kg)' })
+  @ApiProperty({ example: '30.2', description: '골격근량 (kg)', type: String })
   @IsString()
   muscleMass: string;
 
-  @ApiProperty({ example: '20.1', description: '체지방량 (kg)' })
+  @ApiProperty({ example: '20.1', description: '체지방량 (kg)', type: String })
   @IsString()
   bodyFat: string;
 
-  @ApiProperty({ example: '22.8', description: 'BMI (체질량지수)' })
+  @ApiProperty({
+    example: '22.8',
+    description: 'BMI (체질량지수)',
+    type: String,
+  })
   @IsString()
   bmi: string;
 
-  @ApiProperty({ example: '175.0', description: '키 (cm)' })
+  @ApiProperty({ example: '175.0', description: '키 (cm)', type: String })
   @IsString()
   height: string;
 
-  @ApiProperty({ example: '18.5', description: '체지방률 (%)' })
+  @ApiProperty({ example: '18.5', description: '체지방률 (%)', type: String })
   @IsString()
   bodyFatPer: string;
 }
