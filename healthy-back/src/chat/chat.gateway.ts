da5fb4a20fd44ab5694c34727a884c35de8a9c3d
ash <tkdgys1234@gmail.com>
2025-04-15 18:50:26 +0900

    chat.gateway username => userNickname으로 수정

--- healthy-back/src/chat/chat.gateway.ts
+++ healthy-back/src/chat/chat.gateway.ts
@@ -15,7 +15,7 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   server: Server;
 
   // socket.id => { username, room }
-  private users = new Map<string, { username: string; room: string }>();
+  private users = new Map<string, { userNickname: string; room: string }>();
 
   handleConnection(@ConnectedSocket() client: Socket) {
     console.log(`Client connected: ${client.id}`);
@@ -37,29 +37,30 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   // 방 입장 및 유저 등록
   @SubscribeMessage('joinRoom')
   handleJoinRoom(
-    @MessageBody() data: { room: string; username: string },
+    @MessageBody() data: { room: string; userNickname: string },
     @ConnectedSocket() client: Socket,
   ) {
-    const { room, username } = data;
+    const { room, userNickname } = data;
     client.join(room);
-    this.users.set(client.id, { username, room });
+    this.users.set(client.id, { userNickname, room });
 
     // 해당 방 유저 목록 전송
     const roomUsers = this.getUsersInRoom(room);
     this.server.to(room).emit('userList', roomUsers);
 
-    console.log(`${username} joined room: ${room}`);
+    console.log(`${userNickname} joined room: ${room}`);
   }
 
   // 메시지 전송
   @SubscribeMessage('sendMessage')
   handleMessage(
-    @MessageBody() data: { room: string; username: string; message: string },
+    @MessageBody()
+    data: { room: string; userNickname: string; message: string },
     @ConnectedSocket() client: Socket,
   ) {
-    const { room, username, message } = data;
-    console.log(`Message from ${username} in room ${room}: ${message}`);
-    this.server.to(room).emit('receiveMessage', { username, message });
+    const { room, userNickname, message } = data;
+    console.log(`Message from ${userNickname} in room ${room}: ${message}`);
+    this.server.to(room).emit('receiveMessage', { userNickname, message });
   }
 
   // 방 안 유저 리스트 가져오기
@@ -67,7 +68,7 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     const usersInRoom: string[] = [];
     for (const [, value] of this.users) {
       if (value.room === room) {
-        usersInRoom.push(value.username);
+        usersInRoom.push(value.userNickname);
       }
     }
     return usersInRoom;
