3c579f5b675d10b860c3eeb71a6632d6c23febcd
ash <tkdgys1234@gmail.com>
2025-04-19 21:16:01 +0900

    admin 쿠키 secure:false 추가

--- healthy-back/src/admin/admin.controller.ts
+++ healthy-back/src/admin/admin.controller.ts
@@ -33,6 +33,7 @@ export class AdminController {
       httpOnly: false,
       maxAge: 1000 * 60 * 60,
       sameSite: 'none', // 또는 'strict' / 'none'
+      secure: false,
     });
 
     return result;
