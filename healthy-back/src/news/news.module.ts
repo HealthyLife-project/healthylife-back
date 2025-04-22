9a6ec3fab578e288ae3e419f6afdc8521c04831e
ash <tkdgys1234@gmail.com>
2025-04-22 15:29:34 +0900

    수정

--- healthy-back/src/news/news.module.ts
+++ healthy-back/src/news/news.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { NewsService } from './news.service';
-
+import { NewsController } from './news.controller';
 @Module({
   imports: [ConfigModule.forRoot()],
+  controllers: [NewsController],
   providers: [NewsService],
   exports: [NewsService], // 필요 시 export 유지
 })
