a5c57c41d7c0074e7a4577626b30cedcf03cddcc
ash <tkdgys1234@gmail.com>
2025-04-23 16:15:11 +0900

    report 수정

--- healthy-back/src/report/report.service.ts
+++ healthy-back/src/report/report.service.ts
@@ -2,22 +2,33 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, DeleteResult } from 'typeorm';
 import { Report } from 'src/database/entities/report.entity';
+import { User } from 'src/database/entities/user.entity';
 @Injectable()
 export class ReportService {
   constructor(
     @InjectRepository(Report)
     private readonly ReportRepository: Repository<Report>,
+    @InjectRepository(User)
+    private readonly UserR: Repository<User>,
   ) {}
 
-  async create(reportData: Partial<Report>) {
-    await this.ReportRepository.create(reportData);
-
-    return this.ReportRepository.save(reportData);
+  async create(reportData: Partial<any>) {
+    const USER = await this.UserR.findOne({ where: { id: reportData.userId } });
+    const Target = await this.UserR.findOne({
+      where: { id: reportData.reporterId },
+    });
+    if (!USER || !Target) {
+      return 'server ERROR';
+    }
+    const reportd = { report: reportData.report, user: USER, reporter: Target };
+    const data = await this.ReportRepository.create(reportd);
+    return this.ReportRepository.save(data);
   }
 
   async reportGet(): Promise<Report[]> {
-    const report = await this.ReportRepository.find();
-
-    return report;
+    const reports = await this.ReportRepository.find({
+      relations: ['user', 'reporter'],
+    });
+    return reports;
   }
 }
